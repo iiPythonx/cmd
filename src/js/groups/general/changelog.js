02c115f294caba891314f84523297ab0e21447c1
iiPython <ben@iipython.dev>
2025-11-19 22:24:18 -0600

    another reorganize

--- src/js/groups/general/changelog.js
+++ src/js/groups/general/changelog.js
@@ -1,9 +1,11 @@
+import log from "/src/assets/changelog.json";
+
 export const changelog = {
     name: "changelog",
     category: "terminal",
     description: "view the cmd changelog",
     command: async (terminal) => {
-        for (const release of await (await fetch("/assets/changelog.json")).json()) {
+        for (const release of log) {
             await terminal.write(`  ${release.version}\n  ════════════════════════`);
             for (const change of release.changed) await terminal.write(`    * ${change}`);
             await terminal.blank();
